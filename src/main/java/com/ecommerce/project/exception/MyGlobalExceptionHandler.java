982d5a2c595cd6e21b32ecd286d23a516d1af7ac
ashutosh <adityatripathi1011@gmail.com>
2025-07-12 10:20:18 +0530

    added validations

---
+++ src/main/java/com/ecommerce/project/exception/MyGlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.ecommerce.project.exception;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class MyGlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String,String> myMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        Map<String,String> response = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> response.put(error.getField(),error.getDefaultMessage()));
+        return response;
+    }
+}

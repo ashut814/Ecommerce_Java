cc03557d70949b3ebd00bee500e227e38d6dcdd5
ashutosh <adityatripathi1011@gmail.com>
2025-07-12 10:23:09 +0530

    custom error handling

--- src/main/java/com/ecommerce/project/exception/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/project/exception/MyGlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.exception;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,9 +12,9 @@ import java.util.Map;
 public class MyGlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String,String> myMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+    public ResponseEntity<Map<String,String>> myMethodArgumentNotValidException(MethodArgumentNotValidException ex){
         Map<String,String> response = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(error -> response.put(error.getField(),error.getDefaultMessage()));
-        return response;
+        return ResponseEntity.badRequest().body(response);
     }
 }
